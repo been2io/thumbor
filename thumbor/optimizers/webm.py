5e1bfdd3165f505d413a54864197a7474af43605
ben <ben@172-0-1-107.lightspeed.brhmal.sbcglobal.net>
2016-12-29 13:39:58 +0800

    add webm optimizer

---
+++ thumbor/optimizers/webm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+from subprocess import Popen, PIPE
+
+import subprocess
+
+from thumbor.optimizers import BaseOptimizer
+from thumbor.utils import logger
+
+
+class Optimizer(BaseOptimizer):
+
+    def should_run(self, image_extension, buffer):
+        return image_extension in ['.webm']
+
+    def optimize(self, buffer, input_file, output_file):
+
+        command = [
+            self.context.config.WEBM_PATH,
+            input_file,
+            '144',
+            "176",
+            output_file,
+        ]
+        with open(os.devnull) as null:
+            subprocess.call(command, stdin=null)
