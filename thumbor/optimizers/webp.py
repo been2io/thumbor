1dc7b8391f09987cc35ba47d7c213ec485a08550
ben <ben@172-0-1-107.lightspeed.brhmal.sbcglobal.net>
2016-12-29 18:02:49 +0800

    add suffix

--- thumbor/optimizers/webp.py
+++ thumbor/optimizers/webp.py
@@ -3,6 +3,7 @@ import os
 from subprocess import Popen, PIPE
 
 import subprocess
+from tempfile import NamedTemporaryFile
 
 from thumbor.optimizers import BaseOptimizer
 from thumbor.utils import logger
@@ -12,7 +13,25 @@ class Optimizer(BaseOptimizer):
 
     def should_run(self, image_extension, buffer):
         return image_extension in ['.webp']
+    def run_optimizer(self, image_extension, buffer):
+        if not self.should_run(image_extension, buffer):
+            return buffer
 
+        ifile = NamedTemporaryFile(delete=False)
+        ofile = NamedTemporaryFile(suffix="webp",delete=False)
+        try:
+            ifile.write(buffer)
+            ifile.close()
+            ofile.close()
+
+            self.optimize(buffer, ifile.name, ofile.name)
+
+            with open(ofile.name, 'rb') as f:  # reopen with file thats been changed with the optimizer
+                return f.read()
+
+        finally:
+            os.unlink(ifile.name)
+            os.unlink(ofile.name)
     def optimize(self, buffer, input_file, output_file):
 
         command = [
