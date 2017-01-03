c03a79b2f48f7646314c90fa2e74d16067177130
ben <ben@172-0-8-98.lightspeed.wlfrct.sbcglobal.net>
2017-01-03 14:48:06 +0800

    no message

--- thumbor/engines/webp.py
+++ thumbor/engines/webp.py
@@ -1,21 +1,10 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# thumbor imaging service
-# https://github.com/thumbor/thumbor/wiki
-
-# Licensed under the MIT license:
-# http://www.opensource.org/licenses/mit-license
-# Copyright (c) 2011 globo.com thumbor@googlegroups.com
 import os
-from io import BytesIO
 from tempfile import NamedTemporaryFile
 
 import subprocess
-from PIL import Image
 
 from thumbor.engines.pil import Engine as PILEngine
-from thumbor.utils import logger
 
 
 
